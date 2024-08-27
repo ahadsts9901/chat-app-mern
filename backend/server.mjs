23e9c8043c11369c161cd8e6a45017568cfefc86
ahadsts9901 <ahadsts990@gmail.com>
2024-08-27 11:47:35 +0500

    code

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,6 +1,14 @@
-import express from "express"
+import express, { json } from "express"
+import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(morgan("dev"))
+app.use(json())
+app.use(cookieParser())
 
 const PORT = process.env.PORT || 5002
 
