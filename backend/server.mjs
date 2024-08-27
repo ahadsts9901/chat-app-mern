c6947ae24a60ae275b130ea9c16908e959bdffda
ahadsts9901 <ahadsts990@gmail.com>
2024-08-27 11:52:08 +0500

    routes

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,14 +1,25 @@
+import "dotenv/config"
 import express, { json } from "express"
 import morgan from "morgan"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+import authRoutes from "./routes/auth.mjs"
+import profileRoutes from "./routes/profile.mjs"
+import userRoutes from "./routes/users.mjs"
+import chatRoutes from "./routes/chat.mjs"
+
 const app = express()
 
+// middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use(morgan("dev"))
 app.use(json())
 app.use(cookieParser())
+
+// routes
+
+app.use("/api/v1", authRoutes, profileRoutes, userRoutes, chatRoutes)
 
 const PORT = process.env.PORT || 5002
 
