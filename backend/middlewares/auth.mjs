ec416ef460deadf0d09d110f7446e8aa4e542483
ahadsts9901 <ahadsts990@gmail.com>
2024-08-27 12:17:33 +0500

    middlewares

--- backend/middlewares/auth.mjs
+++ backend/middlewares/auth.mjs
@@ -1,3 +1,37 @@
+import "dotenv/config"
+import jwt from "jsonwebtoken"
+import { errorMessages } from "../errorMessages.mjs"
+
 export const authMiddleware = async (req, res, next) => {
+
+    try {
+
+        const { hart } = req?.cookies
+
+        if (!hart) {
+            return res.status(401).send({
+                message: errorMessages?.unAuthError
+            })
+        }
+
+        const currentUser = jwt?.verify(hart, process.env.JWT_KEY)
+
+        if (!currentUser) {
+            return res.status(401).send({
+                message: errorMessages?.unAuthError
+            })
+        }
+
+        req.currentUser = currentUser
+
+        next()
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            message: errorMessages?.serverError,
+            error: error?.message
+        })
+    }
 
 }
--- backend/middlewares/jwtToken.mjs
+++ backend/middlewares/jwtToken.mjs
@@ -1,3 +1,36 @@
+import "dotenv/config"
+import jwt from "jsonwebtoken"
+import { errorMessages } from "../errorMessages.mjs"
+import { sessionInDays } from "../core.mjs"
+
 export const issueJwtTokenMiddleware = async (req, res, next) => {
+
+    try {
+
+        const { loginTokenPayload } = req
+
+        if (!loginTokenPayload) {
+            return res.status(400).send({
+                message: errorMessages.noTokenPayload
+            })
+        }
+
+        const hart = jwt?.sign(loginTokenPayload, process.env.JWT_KEY, { expiresIn: `${sessionInDays}d` })
+
+        res.cookie('hart', hart, {
+            httpOnly: true,
+            secure: true,
+            expires: new Date(Date.now() + sessionInDays * 24 * 60 * 60 * 1000)
+        });
+
+        next()
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            message: errorMessages?.serverError,
+            error: error?.message
+        })
+    }
 
 }
