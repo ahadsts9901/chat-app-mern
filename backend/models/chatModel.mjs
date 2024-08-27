d6e389c0ce59404e6444648f4b1172b5f41a3dc8
ahadsts9901 <ahadsts990@gmail.com>
2024-08-27 12:13:17 +0500

    models

---
+++ backend/models/chatModel.mjs
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+
+let chatSchema = new Schema({
+
+    from_id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+
+    to_id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+
+    text: {
+        type: String,
+        default: null
+    },
+
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+
+    contentUrl: {
+        type: String,
+        default: null,
+        maxlength: 1000,
+    },
+
+});
+
+let chatModel;
+
+try {
+
+    chatModel = model('chats');
+
+} catch (error) {
+
+    chatModel = model('chats', chatSchema);
+
+}
+
+export { chatModel };
