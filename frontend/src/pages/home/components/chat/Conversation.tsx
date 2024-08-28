24af1c7e79011b96d787aab0cecb2f8eb4b887a7
ahadsts9901 <ahadsts990@gmail.com>
2024-08-28 14:11:35 +0500

    code

--- frontend/src/pages/home/components/chat/Conversation.tsx
+++ frontend/src/pages/home/components/chat/Conversation.tsx
@@ -1,10 +1,44 @@
 import "./Main.css"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { baseUrl } from "../../../../utils/core"
+import Message from "./Message"
 
 const Conversation = () => {
+
+    const { userId } = useParams()
+
+    const [messages, setMessages] = useState<any[]>([])
+
+    useEffect(() => {
+
+        if (userId) getMessages(userId)
+
+    }, [userId])
+
+    const getMessages = async (userId: string) => {
+
+        if (!userId || userId?.trim() === "") return
+
+        try {
+
+            const resp = await axios.get(`${baseUrl}/api/v1/messages/${userId}`, { withCredentials: true })
+
+            setMessages(resp?.data?.data)
+
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+
     return (
         <>
             <div className="conversation">
-
+                {
+                    messages?.map((message: any, i: number) => <Message key={i} message={message} />)
+                }
             </div>
         </>
     )
---
+++ frontend/src/pages/home/components/chat/Message.tsx
@@ -0,0 +1,11 @@
+import "./Message.css"
+
+const Message = ({ message }: any) => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Message
