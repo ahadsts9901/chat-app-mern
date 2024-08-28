8fa4ece4ebdc6733844a4a6f828dcf35e5cfde56
ahadsts9901 <ahadsts990@gmail.com>
2024-08-28 13:59:36 +0500

    code

--- frontend/src/pages/home/components/contacts/Contact.tsx
+++ frontend/src/pages/home/components/contacts/Contact.tsx
@@ -1,16 +1,17 @@
 import "./Main.css"
 import { defaultProfilePicture } from "../../../../utils/core"
 import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 const Contact = ({ user }: any) => {
 
     const currentUser = useSelector((state: any) => state?.user)
     const navigate = useNavigate()
+    const { userId } = useParams()
 
     return (
         <>
-            <div className="contact" onClick={() => navigate(`/chat/${user?._id}`)}>
+            <div className={`contact ${userId === user?._id ? "active-contact" : ""}`} onClick={() => navigate(`/chat/${user?._id}`)}>
                 <img src={user?.profilePhoto ? user?.profilePhoto : defaultProfilePicture} alt="profile photo"
                     onError={(e: any) => e.target.src = defaultProfilePicture}
                 />
